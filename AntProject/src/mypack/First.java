bf5e07740371fdbc1762a731a652bda1a8de9b9d
Yeshendra Verma <yeshendra.verma>
2016-11-16 15:59:39 +0530

    changes

--- AntProject/src/mypack/First.java
+++ AntProject/src/mypack/First.java
@@ -7,15 +7,7 @@ public class First {
 	public static void main(String[] args) 
 	{
 		
-		System.out.println("Welcome to the ant tutorial");
-		System.out.println("Added by Priyanka");
-                System.out.println("Added by Priyanka again");
-		System.out.println("Added by Priyanka again again");
-		System.out.println("Added again");
-		System.out.println("Added by me");
-		System.out.println("Added by me again");	
-		System.out.println("Added by me me");
-		System.out.println("Test");
+	System.out.println("Yeshendra");
 	}
 
 }
