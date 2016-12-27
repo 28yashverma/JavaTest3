485792b9728463105bc186b46f05ce934cf9683a
Yeshendra Verma <yeshendra.verma>
2016-12-27 18:36:36 +0530

    Conflict should happen from prabhjeet

--- AntProject/src/mypack/First.java
+++ AntProject/src/mypack/First.java
@@ -9,6 +9,7 @@ public class First {
 		
 	System.out.println("Yeshendra");
 	System.out.println("Prabhjeet + newly changes + made on git hub website");
+	System.out.println("Let this line throw conflict - Prabhjeet");
 	}
 
 }
